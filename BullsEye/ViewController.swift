7ab04c0d2fcbe7a7fa7e6958c7b4e00b286a3ce0
Alex Perucchini <alessandrop65@gmail.com>
2018-10-14 16:36:21 -0600

    UI Layout

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,13 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+	@IBAction func sliderMoved(_ slider: UISlider) {
+		print("The slider has moved \(slider.value)")
+	}
+	
 	@IBAction func showAlert(_ sender: UIButton) {
 		let alert = UIAlertController(title: "Hello, World",
 									  message: "This is my first app!",
 									  preferredStyle: .alert)
-		let action = UIAlertAction(title: "Awesome", style: .default,
-								   handler: nil)
+		let action = UIAlertAction(title: "Awesome", style: .default,handler: nil)
 		alert.addAction(action)
 		present(alert, animated: true, completion: nil)
 	}
