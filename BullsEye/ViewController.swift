beb49718903df2796598b07e2226c30937ee3516
Alex Perucchini <alessandrop65@gmail.com>
2018-10-14 15:42:47 -0600

    Initial Commit iOS Apprentice App 1

---
+++ BullsEye/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BullsEye
+//
+//  Created by Alex Perucchini on 10/14/18.
+//  Copyright © 2018 Ali3nlabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	@IBAction func showAlert(_ sender: UIButton) {
+		let alert = UIAlertController(title: "Hello, World",
+									  message: "This is my first app!",
+									  preferredStyle: .alert)
+		let action = UIAlertAction(title: "Awesome", style: .default,
+								   handler: nil)
+		alert.addAction(action)
+		present(alert, animated: true, completion: nil)
+	}
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+}
+
