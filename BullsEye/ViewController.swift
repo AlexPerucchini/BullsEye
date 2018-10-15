b31ee9cb20f0448620c7eca8c15ef8860f52c136
Alex Perucchini <alessandrop65@gmail.com>
2018-10-14 18:16:01 -0600

    Slider functionality, score and round labels updates

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,21 +9,61 @@
 import UIKit
 
 class ViewController: UIViewController {
+	
+	@IBOutlet weak var slider: UISlider!
+	@IBOutlet weak var roundLabel: UILabel!
+	@IBOutlet weak var scoreLabel: UILabel!
+	
+	var sliderValue: Int = 0
+	var targetValue: Int = 0
+	var roundValue: Int	= 0
+	var scoreValue: Int = 0
+	
 	@IBAction func sliderMoved(_ slider: UISlider) {
-		print("The slider has moved \(slider.value)")
+		sliderValue = lroundf(slider.value)
+		print("The slider has moved \(sliderValue)")
 	}
 	
-	@IBAction func showAlert(_ sender: UIButton) {
-		let alert = UIAlertController(title: "Hello, World",
-									  message: "This is my first app!",
+	@IBAction func playGame(_ sender: UIButton) {
+		let alert = UIAlertController(title: "Slider Value - Target Value",
+									  message: "Slider(\(sliderValue))-Target(\(targetValue))",
 									  preferredStyle: .alert)
 		let action = UIAlertAction(title: "Awesome", style: .default,handler: nil)
 		alert.addAction(action)
 		present(alert, animated: true, completion: nil)
+		self.startNewRound()
 	}
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		self.startNewRound()
+	}
+	
+	func startNewRound() {
+		//set new random target number
+		targetValue = Int.random(in: 1...100)
+		//reset slider value display
+		sliderValue = 50
+		//reset slider bar
+		slider.value = Float(sliderValue)
+		//update round
+		roundValue += 1
+		roundLabel.text = "\(roundValue)"
+		//update score
+		scoreValue += 1
+		scoreLabel.text = "\(scoreValue)"
+		
+	}
+	
+	@IBAction func startOver() {
+		//reset round
+		roundValue = 1
+		roundLabel.text = "\(roundValue)"
+		//reset score
+		scoreValue = 0
+		scoreLabel.text = "\(scoreValue)"
+		//reset slider
+		sliderValue = 50
 	}
 }
 
