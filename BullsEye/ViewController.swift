3e0abcd9be97ef380daccf2dcd02def03fa5ff00
Alex Perucchini <alexp@Alexs-MacBook-Pro.local>
2018-12-06 18:28:39 -0700

    Game logic

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -10,25 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 	
-	@IBOutlet weak var slider: UISlider!
-	@IBOutlet weak var roundLabel: UILabel!
-	@IBOutlet weak var scoreLabel: UILabel!
+	@IBOutlet weak var slider:      UISlider!
+	@IBOutlet weak var roundLabel:  UILabel!
+	@IBOutlet weak var scoreLabel:  UILabel!
 	
-	var sliderValue: Int = 0
-	var targetValue: Int = 0
-	var roundValue: Int	= 0
-	var scoreValue: Int = 0
+	var currentValue:   Int = 0
+	var targetValue:    Int = 0
+	var roundValue:     Int	= 0
+	var scoreValue:     Int = 0
 	
 	@IBAction func sliderMoved(_ slider: UISlider) {
-		sliderValue = lroundf(slider.value)
-		print("The slider has moved \(sliderValue)")
+		currentValue = lroundf(slider.value)
+		print("The slider has moved \(currentValue)")
 	}
 	
 	@IBAction func playGame(_ sender: UIButton) {
-		let alert = UIAlertController(title: "Slider Value - Target Value",
-									  message: "Slider(\(sliderValue))-Target(\(targetValue))",
-									  preferredStyle: .alert)
-		let action = UIAlertAction(title: "Awesome", style: .default,handler: nil)
+        
+        let difference = abs( currentValue - targetValue)
+        let points = 100 - difference
+    
+        //add score
+        scoreValue += points
+		let alert = UIAlertController(title: "Hello Game",
+            message:
+                "Current: \(currentValue)" +
+                "\n Target:  \(targetValue)" +
+                "\n Points: \(points)",
+             preferredStyle: .alert)
+		let action = UIAlertAction(title: "Ok", style: .default,handler: nil)
 		alert.addAction(action)
 		present(alert, animated: true, completion: nil)
 		self.startNewRound()
@@ -43,16 +52,14 @@ class ViewController: UIViewController {
 		//set new random target number
 		targetValue = Int.random(in: 1...100)
 		//reset slider value display
-		sliderValue = 50
+		currentValue = 50
 		//reset slider bar
-		slider.value = Float(sliderValue)
+		slider.value = Float(currentValue)
 		//update round
-		roundValue += 1
+		roundValue += 0
 		roundLabel.text = "\(roundValue)"
 		//update score
-		scoreValue += 1
 		scoreLabel.text = "\(scoreValue)"
-		
 	}
 	
 	@IBAction func startOver() {
@@ -63,7 +70,7 @@ class ViewController: UIViewController {
 		scoreValue = 0
 		scoreLabel.text = "\(scoreValue)"
 		//reset slider
-		sliderValue = 50
+		currentValue = 50
 	}
 }
 
